19eb48e99ff3b739719c500f54f32b4bb72787aa
Nicholas Leacock <code_black@hackermail.com>
2026-05-13 15:34:02 +0400

    feat: change click sound on mode toggle

--- scripts/game.js
+++ scripts/game.js
@@ -20,7 +20,7 @@ const imageCaption = document.getElementById('imageCaption');
 const clickSound = document.getElementById('clickSound');
 
 function toggleMode() {
-    clickSound.play().catch(() => {});
+    levelSound.play().catch(() => {});
     const isBoardVisible = boardSvg.style.display !== 'none';
 
     if (isBoardVisible) {
